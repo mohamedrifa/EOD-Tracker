603e517d7ffb000d807d4e04f176be80f5824384
Mohamed Rifayath <141167029+mohamedrifa@users.noreply.github.com>
2026-03-18 14:33:07 +0530

    Remove env from tracking

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'pages/dashboard.dart';
 import 'pages/login_page.dart';
 import 'pages/signup_page.dart';
 import 'pages/settings_page.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
 import 'utils/auth_guard.dart';
 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await dotenv.load(fileName: ".env"); 
   SharedPreferences prefs = await SharedPreferences.getInstance();
   String? token = prefs.getString("token");
 
